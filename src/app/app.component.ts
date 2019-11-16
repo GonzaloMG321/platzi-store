dda7e0fcb44543b0b2a274bf43bc5b4afcb32e2e
Gonzalo <MendozaGarciaGonzalo>
2019-11-15 18:53:30 -0600

    Haciendo pruebas con directivas ngIf ngFor

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'platzi-store';
+
+  items = ['nicolas', 'julian', 'Gonzalo']
+
+  addItem() {
+    this.items.push('nuevo item');
+  }
+
+  deleteItem(index: number) {
+    this.items.splice(index, 1);
+  }
+}
